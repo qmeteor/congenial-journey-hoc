ba99bf1e2b08e8fe3eb44ac195e70025026151ab
Bien Pham <pham_bv@hotmail.com>
2017-06-13 23:39:49 -0400

    added resources component

---
+++ src/components/resources.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Bien on 2017-06-13.
+ */
+import React, { Component } from 'react';
+
+
+export default () => {
+    return (
+        <div>
+            Super Special Recipe
+            <ul>
+                <li>1 Cup Sugar</li>
+                <li>1 Cup Pepper</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
